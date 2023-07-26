ae4714f0d175df715e342906609ecc933c687816
Guilherme Padilha <guilhermepjobins@hotmail.com>
2023-07-26 16:55:17 -0300

    changes

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    background: '#d3d0dd',
+    primary: {
+      lighter: '#E8E3FF',
+      light: '#6674F4',
+      main: '#5861FC',
+      dark: '#3346F8',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+    },
+    danger: {
+      light: '#f97171',
+      main: '#fc5050',
+      dark: '#f63131',
+    },
+  },
+};
